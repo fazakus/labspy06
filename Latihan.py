03dc9f75d89accfb989ad5c6f855e8282b8e6bf4
Faza Ardan Kusuma <fazkus47@gmail.com>
2020-11-29 23:07:04 +0700

    update program latihan

---
+++ Latihan.py
@@ -0,0 +1,37 @@
+import math
+def a(x):
+    return x**2
+def b(x, y):
+    return math.sqrt(x**2 + y**2)
+def c(*args):
+    return sum(args)/len(args)
+def d(s):
+    return "".join(set(s))
+
+#Dirubah menggunakan Lambda
+
+aa = lambda x : x**2
+bb = lambda x, y : math.sqrt(x**2 + y**2)
+cc = lambda *args : sum(args)/len(args)
+dd = lambda s : "".join(set(s))
+
+#output
+print("Latihan a")
+print("=========")
+print(a(4))
+print(aa(4))
+print()
+print("Latihan b")
+print("=========")
+print(b(4,7))
+print(bb(4,7))
+print()
+print("Latihan c")
+print("=========")
+print(c(10))
+print(cc(10))
+print()
+print("Latihan d")
+print("=========")
+print(d("abcde"))
+print(dd("abcde"))
